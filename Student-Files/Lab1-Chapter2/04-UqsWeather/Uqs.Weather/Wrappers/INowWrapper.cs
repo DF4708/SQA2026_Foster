51d4ba4580e02f022e52fbc3e2257be1a0da7a74
DF4708 <138391381+DF4708@users.noreply.github.com>
2026-02-19 20:31:58 -0600

    Delete Student-Files directory

--- Student-Files/Lab1-Chapter2/04-UqsWeather/Uqs.Weather/Wrappers/INowWrapper.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Uqs.Weather.Wrappers;
-
-public interface INowWrapper
-{
-    // Wrapper around DateTime.Now so callers don't have to depend on the real system clock.
-    // This makes time-based behavior deterministic in unit tests.
-    DateTime Now { get; }
-}
-
--- Student-Files/Lab1-Chapter2/04-UqsWeather/Uqs.Weather/Wrappers/NowWrapper.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Uqs.Weather.Wrappers;
-
-public class NowWrapper : INowWrapper
-{
-    // Centralizing DateTime.Now behind an interface keeps the controller easy to test.
-    // In production this returns the real time; in tests a fake can return a fixed value.
-         public DateTime Now => DateTime.Now;
-}
